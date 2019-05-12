6a1d782169166a8e5a3552d3fe456fac394be53b
Adithya Kamath <adithyakamath96@gmail.com>
2019-05-13 01:45:21 +0530

    Added the Evil Insult API. Generates an alert now! Yet to work on styling

--- IGen/IGen/MainPage.xaml.cs
+++ IGen/IGen/MainPage.xaml.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace IGen
 {
+    public class InsultText
+    {
+        public string number { get; set; }
+        public string language { get; set; }
+        public string insult { get; set; }
+        public string created { get; set; }
+        public string shown { get; set; }
+        public string createdby { get; set; }
+        public string active { get; set; }
+        public string comment { get; set; }
+
+    }
     public partial class MainPage : ContentPage
     {
         public MainPage()
@@ -14,9 +28,24 @@ namespace IGen
             InitializeComponent();
         }
 
-        private void InsultButton_Clicked(object sender, EventArgs e)
+        private async void InsultButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Alert","Test Alert","Ok");
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync("https://evilinsult.com/generate_insult.php?lang=en&type=json"))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        var mycontent = await content.ReadAsStringAsync();
+                        InsultText someText = JsonConvert.DeserializeObject<InsultText>(mycontent);
+                       await DisplayAlert("Insult", someText.insult, "Ok");
+                    }
+                }
+
+            }
+            
         }
+       
+
     }
 }
